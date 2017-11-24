15a56058910675a3bceb178f61421640a7b126a3
Nicolò Jacazio <njacazio@hotmail.com>
2017-11-24 09:20:10 +0100

    Added file lister for xml

---
+++ clustering/listfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$1" == "-h" ]; then
+    echo "Usage: `basename $0`: (1) production (2) runs"
+    exit 0;
+fi
+
+year="20${1:3:2}"
+Path="/alice/sim/${year}/${1}/"
+echo $Path
+# alien_find -x collection "${searchpath} ${what}" > "${listname}"
+for i in ${2}; do
+    command="find -x collection /alice/sim/${year}/${1}/${i}/* AliESDs.root > ${1}_${i}.xml"
+    echo $command
+    sleep 3
+    eval $command
+done
